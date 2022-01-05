e8bdd1e540820cda6f4b24806dde0fe6fe19c70e
Robert Gottlieb <Robert.x.gottlieb@uconn.edu>
2022-01-05 13:08:48 -0500

    Allow subexpressions in binarize!()

--- src/transform/binarize.jl
+++ src/transform/binarize.jl
@@ -4,7 +4,8 @@ Rules for transform narity operations into arity 1 operations. (Works...)
 function binarize!(ex::Expr)
     (arity(ex) < 3) && return ex
     if op(ex) ∈ (:+, :*, :min, :max)
-        new_arg = [op(ex)]
+        new_arg = Vector{Union{Symbol, Expr}}(undef,0)
+        push!(new_arg, op(ex))
         for j=3:(arity(ex) + 1) 
             push!(new_arg, ex.args[j])
         end
