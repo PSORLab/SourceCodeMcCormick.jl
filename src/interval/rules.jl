dd5708bd7f866405b2005f19350d16b34021a4de
Robert Gottlieb <Robert.x.gottlieb@uconn.edu>
2022-01-12 10:49:18 -0500

    Transform rules for * and Symbol

--- src/interval/rules.jl
+++ src/interval/rules.jl
@@ -8,6 +8,11 @@ using IfElse
 #=
 Unitary rules
 =#
+function transform_rule(::IntervalTransform, ::Symbol, yL, yU, xL, xU)
+    rl = Assignment(yL, :($xL))
+    ru = Assignment(yU, :($xU))
+    AssignmentPair(rl, ru)
+end
 function transform_rule(::IntervalTransform, ::typeof(exp), yL, yU, xL, xU)
     rl = Assignment(yL, :(exp($xL)))
     ru = Assignment(yU, :(exp($xU)))
@@ -28,92 +33,23 @@ function transform_rule(::IntervalTransform, ::typeof(-), zL, zU, xL, xU, yL, yU
     AssignmentPair(rl, ru)
 end
 function transform_rule(::IntervalTransform, ::typeof(*), zL, zU, xL, xU, yL, yU)
-    rl = ifelse(yL >= 0.0,
-            ifelse(xL >= 0.0,
-                Assignment(zL, :($xL*$yL)),
-                ifelse(xU <= 0.0,
-                    Assignment(zL, :($xL*$yU)),
-                    Assignment(zL, :($xL*$yU)))),
-            ifelse(yU <= 0.0,
-                ifelse(xL >= 0.0,
-                    Assignment(zL, :($xU*$yL)),
-                    ifelse(xU <= 0.0,
-                        Assignment(zL, :($xU*$yU)),
-                        Assignment(zL, :($xU*$yL)))),
-                ifelse(xL >= 0.0,
-                        Assignment(zL, :($xU*$yL)),
-                        ifelse(xU <= 0.0,
-                            Assignment(zL, :($xL*$yU)),
-                            ifelse(xL*yU < xU*yL,
-                                Assignment(zL, :($xL*$yU)),
-                                Assignment(zL, :($xU*$yL)))))))
-    ru = ifelse(yL >= 0.0,
-        ifelse(xL >= 0.0,
-            Assignment(zU, :($xU*$yU)),
-            ifelse(xU <= 0.0,
-                Assignment(zU, :($xU*$yL)),
-                Assignment(zU, :($xU*$yU)))),
-        ifelse(yU <= 0.0,
-            ifelse(xL >= 0.0,
-                Assignment(zU, :($xL*$yU)),
-                ifelse(xU <= 0.0,
-                    Assignment(zU, :($xL*$yL)),
-                    Assignment(zU, :($xL*$yL)))),
-            ifelse(xL >= 0.0,
-                    Assignment(zU, :($xU*$yU)),
-                    ifelse(xU <= 0.0,
-                        Assignment(zU, :($xL*$yL)),
-                        ifelse(xL*yL > xU*yU,
-                            Assignment(zU, :($xL*$yL)),
-                            Assignment(zU, :($xU*$yU)))))))
+    rl = Assignment(zL, :(ifelse($yL >= 0.0, 
+        ifelse($xL >= 0.0, $xL*$yL,
+            ifelse($xU <= 0.0, $xL*$yU, $xL*$yU)),
+        ifelse($yU <= 0.0,
+            ifelse($xL >= 0.0, $xU*$yL,
+                ifelse($xU <= 0.0, $xU*$yU, $xU*$yL)),
+            ifelse($xL > 0.0, $xU*$yL,
+                ifelse($xU < 0.0, $xL*$yU, min($xU*$yL, $xU*$yU)))))))
+    ru = Assignment(zU, :(ifelse($yL >= 0.0, 
+        ifelse($xL >= 0.0, $xU*$yU,
+            ifelse($xU <= 0.0, $xU*$yL, $xU*$yU)),
+        ifelse($yU <= 0.0,
+            ifelse($xL >= 0.0, $xL*$yU,
+                ifelse($xU <= 0.0, $xL*$yL, $xL*$yL)),
+            ifelse($xL > 0.0, $xU*$yU,
+                ifelse($xU < 0.0, $xL*$yL, max($xL*$yL, $xU*$yU)))))))
     AssignmentPair(rl, ru)
-
-    # Same as above, but in normal if-elseif-else logic
-    # if yL >= 0.0
-    #     if xL >= 0.0
-    #         rl = Assignment(zL, :($xL*$yL))
-    #         ru = Assignment(zU, :($xU*$yU))
-    #     elseif xU <= 0.0
-    #         rl = Assignment(zL, :($xL*$yU))
-    #         ru = Assignment(zU, :($xU*$yL))
-    #     else
-    #         rl = Assignment(zL, :($xL*$yU))
-    #         ru = Assignment(zU, :($xU*$yU))
-    #     end
-    # elseif yU <= 0.0
-    #     if xL >= 0.0
-    #         rl = Assignment(zL, :($xU*$yL))
-    #         ru = Assignment(zU, :($xL*$yU))
-    #     elseif xU <= 0.0
-    #         rl = Assignment(zL, :($xU*$yU))
-    #         ru = Assignment(zU, :($xL*$yL))
-    #     else
-    #         rl = Assignment(zL, :($xU*$yL))
-    #         ru = Assignment(zU, :($xL*$yL))
-    #     end
-    # else
-    #     if xL > 0.0
-    #         rl = Assignment(zL, :($xU*$yL))
-    #         ru = Assignment(zU, :($xU*$yU))
-    #     elseif xU < 0.0
-    #         rl = Assignment(zL, :($xL*$yU))
-    #         ru = Assignment(zU, :($xL*$yL))
-    #     else
-    #         if xL*yU < xU*yL
-    #             rl = Assignment(zL, :($xL*$yU))
-    #         else
-    #             rl = Assignment(zL, :($xU*$yL))
-    #         end
-    #         if xL*yL > xU*yU
-    #             ru = Assignment(zU, :($xL*$yL))
-    #         else
-    #             ru = Assignment(zU, :($xU*$yU))
-    #         end
-    #     end
-    # end
 end
-
-
-
 
 # TODO: /, ^
